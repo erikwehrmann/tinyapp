e6dc6f2f7af2e4bf7a7ffbd116a0489c7316afb7
erikwehrmann <erik.wehrmann@gmail.com>
2022-11-29 22:03:02 +0000

    Added Login and Logout funciton

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cookies = require('cookie-parser');
+app.use(cookies());
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
@@ -46,7 +48,7 @@ app.post('/urls', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render('urls_index', templateVars);
 });
 
@@ -54,12 +56,23 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  const templateVars = { username: req.cookies["username"] };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], newLongURL: ''};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], newLongURL: '', username: req.cookies["username"] };
   res.render('urls_show', templateVars);
 });
 
