a985ee2746ddbd2fe8cb3601245b8cc553eaad5d
erikwehrmann <erik.wehrmann@gmail.com>
2022-12-01 16:37:01 +0000

    Added encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const cookies = require('cookie-parser');
-app.use(cookies());
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}));
 const bcrypt = require('bcryptjs');
 const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true} ));
 
 app.set('view engine', 'ejs');
 
@@ -59,14 +64,12 @@ const urlsForUser = function (id) {
   return usersURL;
 };
 
-app.use(express.urlencoded({ extended: true} ));
-
 app.get('/', (req, res) => {
   res.redirect('/login');
 });
 
 app.get('/register', (req, res) => {
-  if (req.cookies.id) {
+  if (req.session.user_id) {
     res.redirect('/urls');
   }
   res.render('registration');
@@ -76,9 +79,7 @@ app.post('/register', (req, res) => {
   if (req.body.email && req.body.password && checkEmail(req.body.email)) {
   const id = getTiny();
   const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-  res.cookie('id', id);
-  res.cookie('email', req.body.email);
-  res.cookie('password', hashedPassword);
+  req.session.user_id = id;
   users[id] = {
     id,
     email: req.body.email,
@@ -91,7 +92,7 @@ app.post('/register', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  if (req.cookies.id) {
+  if (req.session.user_id) {
     res.redirect('/urls');
   }
   res.render('login');
@@ -101,9 +102,7 @@ app.post('/login', (req, res) => {
   const array = Object.values(users)
   for (const user of array) {
     if (user.email === req.body.email && bcrypt.compareSync(req.body.password, user.password)) {
-      res.cookie('id', user.id);
-      res.cookie('email', user.email);
-      res.cookie('password', user.password);
+      req.session.user_id = user.id;
       res.redirect('/urls');
     }
   }
@@ -112,30 +111,28 @@ app.post('/login', (req, res) => {
 });
 
 app.post('/logout', (req, res) => {
-  res.clearCookie('id');
-  res.clearCookie('email');
-  res.clearCookie('password');
+  req.session = null;
   res.redirect('/login');
 });
 
 app.post('/urls', (req, res) => {
-  if (!req.cookies.id) {
+  if (!req.session.user_id) {
     res.send('Cannot perform this action. Please Login');
   }
   let tinyURL = getTiny();
   urlDatabase[tinyURL] = {
     longURL: req.body.longURL,
-    userID: req.cookies.id
+    userID: req.session.user_id
   }
   res.redirect(`/urls/${tinyURL}`);
 });
 
 app.get('/urls', (req, res) => {
-  if (!req.cookies.id) {
+  if (!req.session.user_id) {
     res.send('Cannot access this page. Please Login');
   }
-  const validURLs = urlsForUser(req.cookies.id);
-  const templateVars = { urls: validURLs, user: users[req.cookies.id] };
+  const validURLs = urlsForUser(req.session.user_id);
+  const templateVars = { urls: validURLs, user: users[req.session.user_id] };
   res.render('urls_index', templateVars);
 });
 
@@ -144,20 +141,20 @@ app.get('/urls.json', (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  if (!req.cookies.id) {
+  if (!req.session.user_id) {
     res.redirect('/login');
   }
-  const templateVars = { user: users[req.cookies.id] };
+  const templateVars = { user: users[req.session.user_id] };
   res.render('urls_new', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  if (!req.cookies.id) {
+  if (!req.session.user_id) {
     res.send('Cannot access this page. Please Login')
   };
   for (const item in urlDatabase) {
-    if (urlDatabase[item]['userID'] === req.cookies.id) {
-      const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]['longURL'], newLongURL: '', user: users[req.cookies.id] };
+    if (urlDatabase[item]['userID'] === req.session.user_id) {
+      const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]['longURL'], newLongURL: '', user: users[req.session.user_id] };
       res.render('urls_show', templateVars);
     }
   }
@@ -165,7 +162,7 @@ app.get('/urls/:id', (req, res) => {
 });
 
 app.post('/urls/:id/edit', (req, res) => {
-  const validURLs = urlsForUser(req.cookies.id);
+  const validURLs = urlsForUser(req.session.user_id);
   if (validURLs[req.params.id] === urlDatabase[req.params.id]) {
     newLongURL = req.body.newLongURL;
     urlDatabase[req.params.id]['longURL'] = newLongURL;
@@ -176,7 +173,7 @@ app.post('/urls/:id/edit', (req, res) => {
 });
 
 app.post('/urls/:id/delete', (req, res) => {
-  const validURLs = urlsForUser(req.cookies.id);
+  const validURLs = urlsForUser(req.session.user_id);
   if (validURLs[req.params.id] === urlDatabase[req.params.id]) {
     delete urlDatabase[req.params.id];
     res.redirect('/urls');
